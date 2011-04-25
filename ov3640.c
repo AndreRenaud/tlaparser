389a7a3ef656a6c446fa23bdb7837ab8998a38ee
andre <andre@f6b31dc6-dd17-0410-bcf4-f92f679a180a>
2011-04-25 21:33:10 +0000

    Old improvements - not sure about these
    git-svn-id: file:///work/internal/svn/tlaparser@2032 f6b31dc6-dd17-0410-bcf4-f92f679a180a

--- ov3640.c
+++ ov3640.c
@@ -10,6 +10,8 @@
  *
  */
 
+#include <signal.h>
+#include <assert.h>
 #include <stdio.h>
 #include <string.h>
 #include <limits.h>
@@ -21,10 +23,10 @@
 
 #define NR_DATA_BITS	8
 
-static int data_pin_mapping[NR_DATA_BITS] = {5, 4, 6, 7, 11, 10, 9, 8, 3, 2};
+//static int data_pin_mapping[NR_DATA_BITS] = {5, 4, 6, 7, 11, 10, 9, 8, 3, 2};
 
 struct ov3640_info {
-    channel_info	*vsync;
+    channel_info	*vref;
     channel_info	*href;
     channel_info	*pclk;
     channel_info	*gpio;
@@ -41,30 +43,50 @@ static struct ov3640_info info;
 
 static void write_pixel_data(capture *c)
 {
-    int i, pixel;
-    int set_bits = 0;
+    int i;
+    unsigned char pixel = 0;
 
     for (i = 0; i < NR_DATA_BITS; i++)
 	if (capture_bit(c, info.data[NR_DATA_BITS - 1 - i]))
 	    pixel |= (1 << i);
 
+    //time_log(c, "pixel: 0x%x\n", pixel);
     fwrite(&pixel, 1, 1, info.fd);
 }
 
 static void parse_capture(capture *c, capture *prev, list_t *channels)
 {
-    static uint64_t this_time, prev_time;
-    int active;
+    static uint64_t prev_time;
+    static int frame_started = 0;
 
+    //time_log(c, "capture: %p\n", c);
     if (!prev)
 	return;
 
-    /* FIXME - Not using vsync currently */
-    active = capture_bit(c, info.gpio);
-    if (active) {
+    /* If we're inside our gpio tagged area.
+     * We might have started half-way through the previous frame, so
+     * wait for a vref transition first
+     */
+    if (capture_bit(c, info.gpio)) {
+        /* When vref transitions, we start/end a frame */
+	if (capture_bit_transition(c, prev, info.vref,
+				   TRANSITION_low_to_high)) {
+            if (info.total_pixels)
+                time_log(c, "Skipping secondary jpeg\n");
+            else {
+                frame_started = 1;
+                info.href_count = 0;
+            }
+        }
+	if (capture_bit_transition(c, prev, info.vref,
+				   TRANSITION_high_to_low)) {
+            frame_started = 0;
+        }
+
 	if (capture_bit_transition(c, prev, info.href,
 				   TRANSITION_low_to_high)) {
 	    /* Start of jpeg data block */
+            time_log(c, "Start of jpeg block\n");
 	    info.href_block_count = 0;
 	    prev_time = capture_time(c);
 	}
@@ -72,16 +94,18 @@ static void parse_capture(capture *c, capture *prev, list_t *channels)
 	if (capture_bit_transition(c, prev, info.href,
 				   TRANSITION_high_to_low)) {
 	    /* End of jpeg data block */
-	    this_time = capture_time(c);
-	    printf("href %d has %d pixels, time since last: %lld\n",
-		   info.href_count, info.href_block_count,
-		   this_time - prev_time);
+	    time_log(c, "href %d has %d pixels, time since last: %lld\n",
+                    info.href_count, info.href_block_count,
+		   capture_time(c) - prev_time);
 	    info.href_count++;
 
 	}
 
-	if (capture_bit(c, info.href) == 1 &&
-	    capture_bit(c, info.vsync) == 1 &&
+        /* If we're inside a frame, href is high & pclk transitions
+           then that is jpeg data, so squirrel it away
+           */
+	if (frame_started &&
+            (capture_bit(c, info.href) == 1) &&
 	    capture_bit_transition(c, prev, info.pclk,
 				   TRANSITION_low_to_high)) {
 	    write_pixel_data(c);
@@ -94,22 +118,24 @@ static void parse_capture(capture *c, capture *prev, list_t *channels)
 void parse_ov3640(bulk_capture *b, char *filename, list_t *channels)
 {
     char name[10];
-    capture *c, *prev;
+    capture *c, *prev = NULL;
     int i;
 
-    info.vsync = capture_channel_details("vref", channels);
+    info.vref = capture_channel_details("vref", channels);
     info.href = capture_channel_details("href", channels);
     info.pclk = capture_channel_details("pclk", channels);
     info.gpio = capture_channel_details("frame", channels);
+    assert(info.vref  && info.href && info.pclk && info.gpio);
     for (i = 0; i < NR_DATA_BITS; i++) {
 	snprintf(name, sizeof(name), "d%d", i);
 	info.data[i] = capture_channel_details(name, channels);
+        assert(info.data[i]);
     }
 
     info.href_count = 0;
     info.href_block_count = 0;
 
-    info.fd = fopen("test.jpg", "w");
+    info.fd = fopen("test.jpg", "wb");
     if (!info.fd) {
 	fprintf(stderr, "Cannot open dump.jpg for writing\n");
 	return;
