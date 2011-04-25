fa46596a063336546eea16357698a3160278a51f
andre <andre@f6b31dc6-dd17-0410-bcf4-f92f679a180a>
2011-04-25 21:32:27 +0000

    Minor cleanups
    git-svn-id: file:///work/internal/svn/tlaparser@2031 f6b31dc6-dd17-0410-bcf4-f92f679a180a

--- sync_from_tla5204.sh
+++ sync_from_tla5204.sh
@@ -3,15 +3,15 @@
 # Handy when you're analysing stuff 'live' 
 # Uses rsync, so you can just keep sync'ing the same directory, and it
 # will only copy what has changed.
-IP=10.168.2.90
+IP=192.168.2.90
 SYNC=$1
 if [ -z "$1" ] ; then
     echo "Enter directory name on TLA5204 to sync (relative to C:/My Documents"
     exit 1
 fi
 BASENAME=`basename "$SYNC"`
-sudo mount -t cifs //$IP/My\ Documents /mnt -n -o user=guest,sec=none,ro
+sudo mount -t cifs //$IP/My\ Documents /mnt -n -o user=guest,sec=none,ro || exit 1
 rsync -rtv "/mnt/$SYNC/" "$BASENAME"
 sudo umount /mnt
 find "$BASENAME" -type f -exec sudo chmod 666 {} \;
-find "$BASENAME" -exec sudo chown andre:andre {} \;
+find "$BASENAME" -exec sudo chown $USER:$USER {} \;
